4ddfd0df61c7f10332bdc2498348313cf09318b3
lorenzovizcaino <lorenzovizcaino@gmail.com>
2023-09-28 08:37:00 +0200

    20230928_08:36

--- src/practica3/LaminaPractica3.java
+++ src/practica3/LaminaPractica3.java
@@ -10,12 +10,12 @@ public class LaminaPractica3 extends JPanel {
     private JLabel labelNumerador1,labelNumerador2,labelDenominador1,LabelDenominador2;
     private JTextField tFieldNum1,tFieldNum2,tFieldDen1,tFieldDen2;
     private JTextArea resultados;
+    private JScrollPane textAreaScroll;
     private JRadioButton suma, resta, multiplicacion, division;
     private JButton calcular, limpiar;
     private GBCConstrains gbc=new GBCConstrains();
-    private Racional2 racional2A;
-    private Racional2 racional2B;
-    private Racional2 racionalResultado;
+    private Racional2 racional2A, racional2B, racionalResultado;
+
 
     public LaminaPractica3(){
         setLayout(new GridBagLayout());
@@ -31,6 +31,8 @@ public class LaminaPractica3 extends JPanel {
         tFieldDen2=new JTextField(10);
 
         resultados=new JTextArea(3,3);
+        textAreaScroll=new JScrollPane(resultados);
+        resultados.setLineWrap(true);
         //resultados.setBackground(Color.LIGHT_GRAY);
         resultados.setBorder(BorderFactory.createTitledBorder("RESULTADOS"));
         JPanel panelOperacion=new JPanel();
@@ -56,24 +58,7 @@ public class LaminaPractica3 extends JPanel {
         calcular=new JButton("Calcular");
         limpiar=new JButton("Limpiar");
 
-//estas lineas comentadas es lo mismo  que las siguintes pero con weightx y weighty a 0.0 (no crecen los componentes al estirar la ventana
-//        add(labelNumerador1,gbc.Constrains(0,0,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//        add(tFieldNum1,gbc.Constrains(1,0,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//        add(labelDenominador1,gbc.Constrains(2,0,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//        add(tFieldDen1,gbc.Constrains(3,0,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//
-//        add(labelNumerador2,gbc.Constrains(0,1,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//        add(tFieldNum2,gbc.Constrains(1,1,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//        add(LabelDenominador2,gbc.Constrains(2,1,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//        add(tFieldDen2,gbc.Constrains(3,1,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//
-//       add(resultados,gbc.Constrains(0,2,3,1,0.0,0.0,GridBagConstraints.BOTH,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//
-//        add(panelOperacion,gbc.Constrains(3,2,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//
-//        add(calcular,gbc.Constrains(2,3,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//        add(limpiar,gbc.Constrains(3,3,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
-//
+
 
         add(labelNumerador1,gbc.Constrains(0,0,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
         add(tFieldNum1,gbc.Constrains(1,0,1,1,1.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
@@ -85,7 +70,8 @@ public class LaminaPractica3 extends JPanel {
         add(LabelDenominador2,gbc.Constrains(2,1,1,1,0.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
         add(tFieldDen2,gbc.Constrains(3,1,1,1,1.0,0.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
 
-        add(resultados,gbc.Constrains(0,2,3,1,1.0,1.0,GridBagConstraints.BOTH,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
+        add(textAreaScroll,gbc.Constrains(0,2,3,1,1.0,1.0,GridBagConstraints.BOTH,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
+
 
         add(panelOperacion,gbc.Constrains(3,2,1,1,1.0,1.0,GridBagConstraints.HORIZONTAL,GridBagConstraints.CENTER,new Insets(5,10,5,10)));
 
@@ -113,7 +99,6 @@ public class LaminaPractica3 extends JPanel {
                 if(suma.isSelected()){
                     racionalResultado.sumar(racional2A,racional2B);
                     if((resultados.getText()).equals("")){
-                        System.out.println("Hola");
                         resultados.setText(String.valueOf(racional2A.devolverNumerador())+"/"+String.valueOf(racional2A.devolverDenominador())+"+"+
                                 String.valueOf(racional2B.devolverNumerador())+"/"+String.valueOf(racional2B.devolverDenominador())+"="+
                                 String.valueOf(racionalResultado.devolverNumerador())+"/"+String.valueOf(racionalResultado.devolverDenominador()));
@@ -142,21 +127,22 @@ public class LaminaPractica3 extends JPanel {
                 if(multiplicacion.isSelected()){
                     racionalResultado.multiplicar(racional2A,racional2B);
                     if((resultados.getText()).equals("")){
-
-                    }else{
                         resultados.setText(String.valueOf(racional2A.devolverNumerador())+"/"+String.valueOf(racional2A.devolverDenominador())+"*"+
                                 String.valueOf(racional2B.devolverNumerador())+"/"+String.valueOf(racional2B.devolverDenominador())+"="+
                                 String.valueOf(racionalResultado.devolverNumerador())+"/"+String.valueOf(racionalResultado.devolverDenominador()));
+
+                    }else{
+                        resultados.setText(resultados.getText()+"\n"+String.valueOf(racional2A.devolverNumerador())+"/"+String.valueOf(racional2A.devolverDenominador())+"*"+
+                                String.valueOf(racional2B.devolverNumerador())+"/"+String.valueOf(racional2B.devolverDenominador())+"="+
+                                String.valueOf(racionalResultado.devolverNumerador())+"/"+String.valueOf(racionalResultado.devolverDenominador()));
                     }
-                    resultados.setText(resultados.getText()+"\n"+String.valueOf(racional2A.devolverNumerador())+"/"+String.valueOf(racional2A.devolverDenominador())+"*"+
-                            String.valueOf(racional2B.devolverNumerador())+"/"+String.valueOf(racional2B.devolverDenominador())+"="+
-                            String.valueOf(racionalResultado.devolverNumerador())+"/"+String.valueOf(racionalResultado.devolverDenominador()));
+
                 }
 
                 if(division.isSelected()){
                     racionalResultado.dividir(racional2A,racional2B);
                     if((resultados.getText()).equals("")){
-                        resultados.setText(resultados.getText()+"\n"+String.valueOf(racional2A.devolverNumerador())+"/"+String.valueOf(racional2A.devolverDenominador())+"/"+
+                        resultados.setText(String.valueOf(racional2A.devolverNumerador())+"/"+String.valueOf(racional2A.devolverDenominador())+"/"+
                                 String.valueOf(racional2B.devolverNumerador())+"/"+String.valueOf(racional2B.devolverDenominador())+"="+
                                 String.valueOf(racionalResultado.devolverNumerador())+"/"+String.valueOf(racionalResultado.devolverDenominador()));
                     }else{
